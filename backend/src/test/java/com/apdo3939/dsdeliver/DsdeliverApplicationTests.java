d4173b525f67ada16908443c5a2e0fe88cdb3417
apdo13 <apdo3939@hotmail.com>
2021-01-04 13:01:19 -0300

    Project created

---
+++ backend/src/test/java/com/apdo3939/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apdo3939.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
