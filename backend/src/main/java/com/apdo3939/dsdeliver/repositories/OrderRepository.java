0ffa5d268ec40e39a07d1bf02f660de86709e299
apdo13 <apdo3939@hotmail.com>
2021-01-05 16:06:05 -0300

    GET /orders

--- backend/src/main/java/com/apdo3939/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/apdo3939/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,19 @@
 package com.apdo3939.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.apdo3939.dsdeliver.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+	
+	/*List<Order> findAllByOrderByMomentAsc();*/
+	
+	
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
+	
 }
