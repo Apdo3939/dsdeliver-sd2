64a3a2a36852e0da1da2762e01f4b0427fe0dd9a
apdo13 <apdo3939@hotmail.com>
2021-01-07 13:31:55 -0300

    Add home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-import Hello from './Hello'
-import Counter from './Counter'
 import Navbar from './Navbar';
+import Home from './Home'
 
 function App() {
   return (
     <div >
       <Navbar/>
+      <Home/>
     </div>
   );
 }
